249ee14c773bb17a54b23f74c053f238eb97aab7
AM <adam@photosynth.ca>
2021-05-26 13:24:23 +0200

    Update plugin meta

--- gen-ecovillages-api-server.php
+++ gen-ecovillages-api-server.php
@@ -1,10 +1,9 @@
 <?php
 /*
-Plugin Name: GEN Ecovillages API
+Plugin Name: GEN Ecovillages API Server
 Plugin URI: https://github.com/Photosynthesis/gen-ecovillages-api-server
 Description: Provide GEN project data for regional network websites
 Version: 0.0.1
-Author: Adam McKenty/Photosynthesis
 */
 
 require_once plugin_dir_path( __FILE__ ) . 'includes/ecovillages-api-server.class.php';
